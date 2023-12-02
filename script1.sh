fd8ae097040e8f96d72eb965f31694bd4db38aeb
tord-otee <138652303+tord-otee@users.noreply.github.com>
2023-12-02 16:50:22 +0100

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
-apt update
-apt upgrade
-
-apt install vim,tmux -y
-
 set -eu -o pipefail # fail on error and report it, debug all lines
 
 sudo -n true
 test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
 
+apt update
+apt upgrade
+
 echo installing the must-have pre-requisites
 while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
-    perl
-    zip unzip
-    exuberant-ctags
-    mutt
-    libxml-atom-perl
-    postgresql-9.6
-    libdbd-pgsql
+    vim
+    tmux
     curl
     wget
-    libwww-curl-perl
 EOF
 )
 
@@ -31,4 +23,5 @@ echo hit Ctrl+C to quit
 echo -e "\n"
 sleep 6
 
-sudo apt-get install -y tig
+echo just kidding nothing happened
+#sudo apt-get install -y tig
