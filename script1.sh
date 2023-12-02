c6f130d79b2efd86fa9a6426a07fa677c0cde3de
tord-otee <138652303+tord-otee@users.noreply.github.com>
2023-12-02 21:02:02 +0100

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -5,7 +5,7 @@ sudo -n true
 test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
 
 apt update
-apt upgrade
+apt upgrade -y
 
 echo installing the must-have pre-requisites
 while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
